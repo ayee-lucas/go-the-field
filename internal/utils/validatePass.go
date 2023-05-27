efcd5dd3d6c0002d7b555c0a5647e1a5ffd52bfc
Alan Lopez <alopez-2018459@kinal.edu.gt>
2023-05-27 12:44:44 -0600

    added bcrypt

---
+++ internal/utils/validatePass.go
@@ -0,0 +1,16 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return "", err
+	}
+	return string(hashed), nil
+}
+
+func VerifyPassword(password, hashed string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashed), []byte(password))
+}
