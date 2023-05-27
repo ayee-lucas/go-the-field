0dba204745cf91ed349550cf62d6a46242b433d2
Alan Lopez <alopez-2018459@kinal.edu.gt>
2023-05-27 12:44:34 -0600

    added env handler

---
+++ internal/utils/env.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() error {
+	prod := os.Getenv("PROD")
+
+	if prod != "true" {
+		envPath := filepath.Join("..", ".env")
+
+		err := godotenv.Load(envPath)
+		if err != nil {
+			return err
+		}
+
+	}
+
+	return nil
+}
