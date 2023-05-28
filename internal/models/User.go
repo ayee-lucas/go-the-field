4d440a6c46ef3ec25803f64302bf6c482260a8e5
Alan Lopez <alopez-2018459@kinal.edu.gt>
2023-05-28 16:49:29 -0600

    Fixed User struct

--- internal/models/User.go
+++ internal/models/User.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 type User struct {
-	ID        string    `json: "id,omitempty" bson: "_id,omitempty"`
-	USERNAME  string    `json: "username" bson:  "username"`
-	EMAIL     string    `json: "email" bson: "email"`
-	PASSWORD  string    `json: "password,omitempty" bson: "password,omitempty"`
-	CreatedAt time.Time `json: "created_at,omitempty" bson: "created_at,omitempty"`
-	UpdatedAt time.Time `json: "updated_at,omitempty" bson: "updated_at,omitempty"`
+	ID        string    `json:"id,omitempty" bson:"_id,omitempty"` // omitempty is used to omit empty fields in json
+	Username  string    `json:"username" bson:"username"`          // omitempty is used to omit empty fields in json
+	Email     string    `json:"email" bson:"email"`
+	Password  string    `json:"password,omitempty" bson:"password,omitempty"`
+	CreatedAt time.Time `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	UpdatedAt time.Time `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
 }
