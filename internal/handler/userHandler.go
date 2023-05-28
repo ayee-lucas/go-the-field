c1fa3eb5b8b53a6143051bd130be391fe35b8cca
Alan Lopez <alopez-2018459@kinal.edu.gt>
2023-05-28 16:49:17 -0600

    Moved database functions to repository

--- internal/handler/userHandler.go
+++ internal/handler/userHandler.go
@@ -1,51 +1,33 @@
 package handler
 
 import (
-	"fmt"
 	"strings"
 	"time"
 
-	"github.com/alopez-2018459/go-bank-system/internal/db"
 	"github.com/alopez-2018459/go-bank-system/internal/models"
-	"github.com/alopez-2018459/go-bank-system/internal/utils"
+	"github.com/alopez-2018459/go-bank-system/internal/repository"
+	"github.com/alopez-2018459/go-bank-system/internal/utils/validations"
 	"github.com/gofiber/fiber/v2"
-	"go.mongodb.org/mongo-driver/bson"
 )
 
 func GetUsers(ctx *fiber.Ctx) error {
 
-	coll := db.GetDBCollection("users")
-
-	users := make([]models.User, 0)
-
-	cursor, err := coll.Find(ctx.Context(), bson.M{})
+	users, err := repository.GetAllUsers()
 
 	if err != nil {
 		return ctx.Status(500).JSON(fiber.Map{"error": err.Error()})
 	}
-
-	for cursor.Next(ctx.Context()) {
-		user := models.User{}
-		err := cursor.Decode(&user)
-		if err != nil {
-			return ctx.Status(500).JSON(fiber.Map{"error": err.Error()})
-		}
-		users = append(users, user)
-	}
-
 	if len(users) == 0 {
 		return ctx.Status(404).JSON(fiber.Map{"error": "No users found"})
-
 	}
 
 	return ctx.Status(200).JSON(users)
-
 }
 
 type createUser struct {
-	USERNAMWE string    `json:"username" bson:"username"`
-	EMAIL     string    `json:"email" bson:"email,"`
-	PASSWORD  string    `json:"password" bson:"password"`
+	Username  string    `json:"username" bson:"username"`
+	Email     string    `json:"email" bson:"email,"`
+	Password  string    `json:"password" bson:"password"`
 	CreatedAt time.Time `json:"created_at" bson:"created_at"`
 	UpdatedAt time.Time `json:"updated_at" bson:"updated_at"`
 }
@@ -53,30 +35,66 @@ type createUser struct {
 func SaveUser(ctx *fiber.Ctx) error {
 	body := new(createUser)
 
-	err := ctx.BodyParser(&body)
+	err := ctx.BodyParser(body)
 
-	if err := ctx.BodyParser(body); err != nil {
-		return ctx.Status(400).JSON(fiber.Map{"error": "Invalid Body"})
+	if err != nil {
+		return ctx.Status(400).JSON(fiber.Map{"error": err.Error()})
 	}
 
-	if body.EMAIL == "" || body.PASSWORD == "" || body.USERNAMWE == "" {
-		return ctx.Status(400).JSON(fiber.Map{"error": "Missing fields"})
+	err = validations.IsStringEmpty(body.Username)
+
+	if err != nil {
+		return ctx.Status(400).JSON(fiber.Map{"error": "Username is required"})
 	}
-	body.PASSWORD, err = utils.HashPassword(body.PASSWORD)
+
+	err = validations.IsStringEmpty(body.Email)
+	if err != nil {
+		return ctx.Status(400).JSON(fiber.Map{"error": "Email is required"})
+	}
+
+	err = validations.IsStringEmpty(body.Password)
+
+	if err != nil {
+		return ctx.Status(400).JSON(fiber.Map{"error": "Password is required"})
+	}
+
+	err = validations.IsPasswordValid(body.Password)
+
+	if err != nil {
+		return ctx.Status(400).JSON(fiber.Map{"error": err.Error()})
+
+	}
+
+	body.Password, err = validations.HashPassword(body.Password)
 
 	if err != nil {
 		return ctx.Status(500).JSON(fiber.Map{"error": "Error hashing password"})
 	}
 
-	body.USERNAMWE = strings.ToLower(body.USERNAMWE)
-	body.EMAIL = strings.ToLower(body.EMAIL)
-	body.CreatedAt = time.Now()
-	body.UpdatedAt = time.Now()
+	body.Username = strings.ToLower(body.Username)
+	body.Email = strings.ToLower(body.Email)
 
-	fmt.Println(body)
+	usernameExist, _ := repository.GetyByUsername(body.Username)
 
-	coll := db.GetDBCollection("users")
-	result, err := coll.InsertOne(ctx.Context(), body)
+	if usernameExist != nil {
+		return ctx.Status(400).JSON(fiber.Map{"error": "Username already exists"})
+	}
+
+	emailExist, _ := repository.GetByEmail(body.Email)
+
+	if emailExist != nil {
+		return ctx.Status(400).JSON(fiber.Map{"error": "Email already exists"})
+	}
+
+	user := &models.User{
+		Username:  body.Username,
+		Email:     body.Email,
+		Password:  body.Password,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	id, err := repository.SaveUser(user)
 
 	if err != nil {
 		return ctx.Status(500).JSON(fiber.Map{"error": "Error saving user", "message": err.Error()})
@@ -84,8 +102,9 @@ func SaveUser(ctx *fiber.Ctx) error {
 
 	return ctx.Status(201).JSON(fiber.Map{
 		"result": fiber.Map{
-			"id":   result,
-			"user": body,
+			"message": "User created successfully",
+			"id":      id,
+			"user":    user,
 		},
 	})
 
