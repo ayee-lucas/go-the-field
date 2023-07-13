1e3edbd5d95dec60c6e5efb1156f0c4fa4a19d47
Alan Lopez <alopez-2018459@kinal.edu.gt>
2023-07-12 21:12:49 -0600

    Added data tu User struct

--- internal/models/User.go
+++ internal/models/User.go
@@ -8,17 +8,18 @@ import (
 
 // omitempty is used to omit empty fields in json
 type User struct {
-	ID        string               `json:"id,omitempty" bson:"_id,omitempty"`
-	Name      string               `json:"name,omitempty" bson:"name,omitempty"`
+	ID        primitive.ObjectID   `json:"id,omitempty" bson:"_id,omitempty"`
+	Name      string               `json:"name,omitempty" bson:"name"`
 	Username  string               `json:"username" bson:"username"`
 	Email     string               `json:"email" bson:"email"`
 	Password  string               `json:"password" bson:"password"`
-	Online    bool                 `json:"online,omitempty" bson:"online,omitempty"`
+	Online    bool                 `json:"online,omitempty" bson:"online"`
 	Role      string               `json:"role" bson:"role" default:"user"`
-	Bio       string               `json:"bio,omitempty" bson:"bio,omitempty"`
-	Likes     []primitive.ObjectID `json:"likes,omitempty" bson:"likes,omitempty"`
-	Followers []primitive.ObjectID `json:"followers,omitempty" bson:"followers,omitempty"`
-	Posts     []primitive.ObjectID `json:"posts,omitempty" bson:"posts,omitempty"`
-	CreatedAt time.Time            `json:"created_at,omitempty" bson:"created_at,omitempty"`
-	UpdatedAt time.Time            `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
+	Bio       string               `json:"bio,omitempty" bson:"bio"`
+	Picture   string               `json:"picture,omitempty" bson:"picture"`
+	Likes     []primitive.ObjectID `json:"likes,omitempty" bson:"likes"`
+	Followers []primitive.ObjectID `json:"followers,omitempty" bson:"followers"`
+	Posts     []primitive.ObjectID `json:"posts,omitempty" bson:"posts"`
+	CreatedAt time.Time            `json:"created_at" bson:"created_at"`
+	UpdatedAt time.Time            `json:"updated_at" bson:"updated_at"`
 }
