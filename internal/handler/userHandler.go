d3cd1eece7366be97cb08931ab8dd1a669632c10
Alan Lopez <alopez-2018459@kinal.edu.gt>
2023-07-20 10:01:21 -0600

    update picture route handler added

--- internal/handler/userHandler.go
+++ internal/handler/userHandler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"github.com/alopez-2018459/go-the-field/internal/auth"
 	"github.com/alopez-2018459/go-the-field/internal/models"
 	"github.com/alopez-2018459/go-the-field/internal/repository"
 	"github.com/alopez-2018459/go-the-field/internal/utils/validations"
@@ -95,5 +96,79 @@ func FinishProfile(ctx *fiber.Ctx) error {
 
 	result, err = repository.UpdateUser(id, data)
 
-	return ctx.Status(200).JSON(fiber.Map{"message": "user found", "user": result})
+	return ctx.Status(200).JSON(fiber.Map{"message": "success", "user": result})
+}
+
+type updatePicture struct {
+	Picture *models.Picture `json:"picture" bson:"picture"`
+}
+
+func UpdatePicture(ctx *fiber.Ctx) error {
+	var result *mongo.UpdateResult
+
+	param := ctx.Params("id")
+
+	body := new(updatePicture)
+
+	sessionHeader := ctx.Get("Authorization")
+
+	if sessionHeader == "" || len(sessionHeader) < 8 || sessionHeader[:7] != "Bearer " {
+		return ctx.Status(401).JSON(fiber.Map{"error": "Invalid header"})
+	}
+
+	sessionId := sessionHeader[7:]
+
+	_, err := auth.GetSession(sessionId)
+
+	if err != nil {
+		return ctx.Status(500).JSON(fiber.Map{"error": "Failed to get session", "message": err.Error(), "status": "unauthenticated"})
+	}
+
+	id, err := primitive.ObjectIDFromHex(param)
+
+	if err != nil {
+		return ctx.Status(400).JSON(fiber.Map{"error": err.Error(), "message": "Invalid Id"})
+	}
+
+	_, err = repository.GetUserById(param)
+
+	if err != nil {
+		return ctx.Status(500).JSON(fiber.Map{"error": err.Error(), "message": "Failed to get user"})
+	}
+	err = ctx.BodyParser(body)
+
+	if err != nil {
+		return ctx.Status(400).JSON(fiber.Map{"error": "Failed to parse request body", "message": err.Error()})
+	}
+
+	err = validations.IsStringEmpty(body.Picture.PictureKey)
+
+	if err != nil {
+		return ctx.Status(400).JSON(fiber.Map{"error": err.Error(), "message": "PictureKey is required"})
+	}
+
+	err = validations.IsStringEmpty(body.Picture.PictureURL)
+
+	if err != nil {
+		return ctx.Status(400).JSON(fiber.Map{"error": err.Error(), "message": "PictureURL is required"})
+	}
+
+	data := bson.D{{Key: "picture", Value: bson.D{{Key: "pictureKey", Value: body.Picture.PictureKey}, {Key: "pictureURL", Value: body.Picture.PictureURL}}}}
+
+	result, err = repository.UpdateUser(id, data)
+
+	if err != nil {
+		return ctx.Status(500).JSON(fiber.Map{"error": err.Error(), "message": "Failed to update user"})
+	}
+
+	sessionData := bson.D{{Key: "picture", Value: bson.D{{Key: "pictureKey", Value: body.Picture.PictureKey}, {Key: "pictureURL", Value: body.Picture.PictureURL}}}}
+
+	_, err = repository.UpdateSession(sessionId, sessionData)
+
+	if err != nil {
+		return ctx.Status(400).JSON(fiber.Map{"error": err.Error(), "message": "Failed to update session"})
+	}
+
+	return ctx.Status(200).JSON(fiber.Map{"message": "success", "user": result})
+
 }
