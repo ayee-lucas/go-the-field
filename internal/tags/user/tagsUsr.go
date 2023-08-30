ef48e323d966015a84e489b3ec4966bd6594ac20
Alan Lopez <nouminosxz@gmail.com>
2023-08-30 00:05:24 -0600

    added tags

---
+++ internal/tags/user/tagsUsr.go
@@ -0,0 +1,23 @@
+package tags_user
+
+const (
+	/** Tags */
+	TAG_USER        = "[USER]"
+	TAG_SESSION     = "[SESSION]"
+	TAG_NAME        = "[NAME]"
+	TAG_BIO         = "[BIO]"
+	TAG_PICTURE     = "[PICTURE]"
+	TAG_PICTURE_KEY = "[PICTURE_KEY]"
+	TAG_PICTURE_URL = "[PICTURE_URL]"
+	TAG_COUNTRY     = "[COUNTRY]"
+	TAG_CITY        = "[CITY]"
+	TAG_SPORTS      = "[SPORTS]"
+	TAG_ORG         = "[ORG]"
+	TAG_NATIONALITY = "[NATIONALITY]"
+	TAG_GENDER      = "[GENDER]"
+	TAG_HEIGHT      = "[HEIGHT]"
+	TAG_WEIGHT      = "[WEIGHT]"
+	TAG_USERNAME    = "[USERNAME]"
+	TAG_EMAIL       = "[EMAIL]"
+	TAG_PASS        = "[PASSWORD]"
+)
