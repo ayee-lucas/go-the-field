03bbb8e7428097529182c9fe0a1d34f785108e3a
Alan Lopez <nouminosxz@gmail.com>
2023-08-30 00:01:40 -0600

    Added constants, for responses

--- internal/responses/errors.go
+++ internal/responses/errors.go
@@ -7,7 +7,9 @@ const (
 	DATA_NOT_FOUND       = "The requested data could not be located "
 	PARSE_BODY_ERROR     = "Failed to parse request body "
 	UPDATE_DATA_ERROR    = "Failed to update data "
+	INVALID_INPUT        = "Invalid input data "
 	INVALID_HEADER_ERROR = "Invalid session header "
+	INVALID_EMAIL_FORMAT = "Invalid email format "
 
 	/** USER ERRORS */
 	PROFILE_FINISHED_ERROR = "Profile Completion Conflict "
